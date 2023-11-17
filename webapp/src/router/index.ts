9578187fb305940ba41c61fd6c73a71c0d7fd2ee
Marco DeBortoli <marco@mdeb.me>
2023-11-17 19:49:10 +0000

    Starting on DashboardView - checkpoint

--- webapp/src/router/index.ts
+++ webapp/src/router/index.ts
@@ -1,23 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import DashboardView from '../views/DashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      redirect: '/dashboard'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: DashboardView,
+      meta: {
+        title: 'Dashboard'
+      }
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  document.title = to.name ? to.name.toString() : 'Ledger';
+  next();
+});
 
 export default router
---
+++ webapp/src/views/DashboardView.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { computed, onMounted, ref } from 'vue';
+
+const selectedDate = ref(new Date());
+
+const displayDate = computed(() => {
+  const monthName = Intl.DateTimeFormat('en', {month: 'long'}).format(selectedDate.value);
+  const year = selectedDate.value.getFullYear();
+
+  return monthName + " " + year;
+})
+
+onMounted(() => {
+  selectedDate.value = new Date(
+    selectedDate.value.getFullYear(), selectedDate.value.getMonth(), 1
+  );
+})
+
+</script>
+
+<template>
+  <div class="flex flex-col w-full h-full">
+
+    <!--Top header which includes the month and net worth component-->
+    <header class="flex justify-center items-center h-12 mb-2 bg-slate-400">
+      <div class="text-3xl uppercase font-bold">
+        {{ displayDate }}
+      </div>
+    </header>
+
+    <section class="grid grid-cols-4 h-screen gap-2">
+      <div class="col-span-3 bg-green-300">TRANSACTIONS</div>
+      <div class="col-span-1 bg-blue-300">ACCOUNTS</div>
+    </section>
+  </div>
+</template>

229fb4a6a6d6dc935f62bedd731272fc6742d3ce
Marco DeBortoli <marco@mdeb.me>
2023-11-17 22:18:00 +0000

    Updated month selector

---
+++ webapp/src/components/MonthNavigation.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+const leftHover = ref(false);
+const rightHover = ref(false);
+defineEmits(['dec', 'inc'])
+const props = defineProps<{
+  date: Date
+}>()
+
+const displayDate = computed(() => {
+  const monthName = Intl.DateTimeFormat('en', {month: 'long'}).format(props.date);
+  const year = props.date.getFullYear();
+  return monthName + " " + year;
+})
+
+</script>
+
+<template>
+  <div class="flex">
+    <i
+      class="ti text-2xl mr-2 hover:cursor-pointer"
+      :class="{'ti-arrow-big-left': !leftHover, 'ti-arrow-big-left-filled': leftHover}"
+      v-on:mouseenter="leftHover=true"
+      v-on:mouseleave="leftHover=false"
+      v-on:click="$emit('dec')"
+    ></i>
+    <div class="text-3xl uppercase font-bold">
+      {{ displayDate }}
+    </div>
+    <i
+      class="ti text-2xl ml-2 hover:cursor-pointer"
+      :class="{'ti-arrow-big-right': !rightHover, 'ti-arrow-big-right-filled': rightHover}"
+      v-on:mouseenter="rightHover=true"
+      v-on:mouseleave="rightHover=false"
+      v-on:click="$emit('inc')"
+    ></i>
+  </div>
+</template>
--- webapp/src/views/DashboardView.vue
+++ webapp/src/views/DashboardView.vue
@@ -1,20 +1,28 @@
 <script setup lang="ts">
-import { computed, onMounted, ref } from 'vue';
+import MonthNavigation from '@/components/MonthNavigation.vue';
+import { ref, onMounted } from 'vue';
 
-const selectedDate = ref(new Date());
+const currentDate = ref(new Date());
 
-const displayDate = computed(() => {
-  const monthName = Intl.DateTimeFormat('en', {month: 'long'}).format(selectedDate.value);
-  const year = selectedDate.value.getFullYear();
+// Functions to deal with the date / month that is currently being displayed
 
-  return monthName + " " + year;
-})
+function decDate() {
+  const copyDate = new Date(currentDate.value);
+  copyDate.setMonth(copyDate.getMonth() - 1);
+  currentDate.value = copyDate;
+}
+
+function incDate() {
+  const copyDate = new Date(currentDate.value);
+  copyDate.setMonth(copyDate.getMonth() + 1);
+  currentDate.value = copyDate;
+}
 
 onMounted(() => {
-  selectedDate.value = new Date(
-    selectedDate.value.getFullYear(), selectedDate.value.getMonth(), 1
+  currentDate.value = new Date(
+    currentDate.value.getFullYear(), currentDate.value.getMonth(), 1
   );
-})
+});
 
 </script>
 
@@ -22,15 +30,19 @@ onMounted(() => {
   <div class="flex flex-col w-full h-full">
 
     <!--Top header which includes the month and net worth component-->
-    <header class="flex justify-center items-center h-12 mb-2 bg-slate-400">
-      <div class="text-3xl uppercase font-bold">
-        {{ displayDate }}
-      </div>
+    <header
+      class="flex justify-center items-center h-12 mb-2 border rounded-md border-black/30"
+    >
+      <MonthNavigation
+        :date="currentDate"
+        @inc="() => incDate()"
+        @dec="() => decDate()"
+      />
     </header>
 
-    <section class="grid grid-cols-4 h-screen gap-2">
-      <div class="col-span-3 bg-green-300">TRANSACTIONS</div>
-      <div class="col-span-1 bg-blue-300">ACCOUNTS</div>
+    <section class="grid grid-cols-1 xl:grid-cols-4 h-screen gap-2">
+      <div class="xl:col-span-3 border rounded-md border-black/30">{{ currentDate }}</div>
+      <div class="xl:col-span-1 border rounded-md border-black/30">ACCOUNTS</div>
     </section>
   </div>
 </template>
